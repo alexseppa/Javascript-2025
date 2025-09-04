8316582a8a8dda2d3615e8354eddbf89e885db2f
alexseppa <als00050@laurea.fi>
2025-09-04 14:52:27 +0300

    Javascript 2025 workshop 1

---
+++ workshop1 scriptit.js
@@ -0,0 +1,25 @@
+alert("Hello world!"); // Press Shift+Enter here
+alert("Another hello world"); // Press Shift+Enter here
+alert("Still one more hello world"); // Enter here
+
+window.alert()
+document.write()
+console.log()
+var x = 5;
+var y = 6;
+var z = x + y;
+console.log(z);
+alert(z);
+document.write(z);
+
+var x = 50;
+var order = 'Beer';
+if (x > 50) {
+document.write("He's over 50!" );
+} else if (x <= 50 && x> 30){
+document.write("Middle aged man, who ordered some "+order);
+} else {
+document.write("It seems you're bit underaged.");
+}
+
+
