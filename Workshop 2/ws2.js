e5675615ff10483d75d298a4fac477cf7e158007
alexseppa <als00050@laurea.fi>
2025-09-17 13:05:37 +0300

    workshop2

---
+++ Workshop 2/ws2.js
@@ -0,0 +1,10 @@
+document.write(`<em> <strong>"If I had nine hours to chop down a tree, I'd spend the first six sharpening my ax."</em> </strong> <p>
+
+– Abraham Lincoln`)
+
+let text;
+if (confirm("Oletko opiskelija?") == true) {
+    text = "Tervetuloa opiskelija";
+} else {
+  text = "Valitettavasti tämä sivu on tarkoitettu vain opiskelijoille";
+}
